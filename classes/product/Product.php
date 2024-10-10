8c43a6fa5826b451e0f986b0d164a25d4607b3ba
armandotruglio <armando.truglio1@gmail.com>
2024-10-10 16:48:06 +0200

    Created Product

--- classes/product/Product.php
+++ classes/product/Product.php
@@ -1,10 +1,41 @@
 <?php
-require_once "../category/Category.php";
+require_once "./classes/category/Category.php";
 
 class Product
 {
+    protected string $type = "Prodotto Generico";
     protected string $title;
     protected string $image;
     protected string $price;
     protected Category $category;
+
+
+    public function __construct(string $_title, string $_image, string $_price, Category $_category)
+    {
+        $this->title = $_title;
+        $this->image = $_image;
+        $this->price = $_price;
+        $this->category = $_category;
+    }
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+    public function getImage(): string
+    {
+        return $this->image;
+    }
+    public function getPrice(): string
+    {
+        return $this->price;
+    }
+    public function getCategory(): Category
+    {
+        return $this->category;
+    }
 }
--- index.php
+++ index.php
@@ -3,3 +3,27 @@
 require_once __DIR__ . "/classes/category/Category.php";
 require_once __DIR__ . "/classes/category/Cats.php";
 require_once __DIR__ . "/classes/category/Dogs.php";
+require_once __DIR__ . "/classes/product/Product.php";
+
+
+$product = new Product("Spazzola", "https://www.wowgrooming.co.uk/cdn/shop/articles/Long_Pin_Brush_800x.png?v=1620598039", "15,55$", new Dogs());
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?= $product->getTitle() ?></h1>
+    <h3><?= $product->getType() ?></h3>
+    <img src="<?= $product->getImage() ?>" alt="<?= $product->getTitle() ?>">
+    <h5><?= $product->getPrice() ?></h5>
+</body>
+
+</html>
